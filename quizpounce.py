28b33e666d5210cac021c85938a7d0e3af14e73d
zubairabid <zubairabid1999+github@gmail.com>
2020-03-26 15:15:53 +0530

    Basic clean version

--- quizpounce.py
+++ quizpounce.py
@@ -15,26 +15,6 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-@bot.command(name="insult", help="Responds with an insult to a passed name")
-async def insulter(ctx, name):
-    response = name + " is sad person"
-    print(ctx)
-    await ctx.send(response)
-
 @bot.command(name="pounce", help="Sends message to the desired channel")
 async def pounce(ctx, *args, **kwargs):
     channel = bot.get_channel(692658879972245514)
